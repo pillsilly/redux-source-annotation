d33f026513d1c96539b1142cb1d832976afc6168
yujingwyh <yujingwyh@163.com>
2019-10-17 14:16:32 +0800

    更新

--- src/utils/isPlainObject.ts
+++ src/utils/isPlainObject.ts
@@ -3,7 +3,7 @@
  * @returns True if the argument appears to be a plain object.
  */
 //判断是否为朴素对象 也就是纯对象
-//朴素对象：原型链上直接继承Object.prototype的对象，即是否用对象直接量{}或new Object()创建的对象
+//朴素对象：原型链上直接继承Object.prototype的对象，即是否用对象字面量{}或new Object()创建的对象
 export default function isPlainObject(obj: any): boolean {
   if (typeof obj !== 'object' || obj === null) return false
 
